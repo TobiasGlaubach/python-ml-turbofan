3eb4916130cfa432f39216129c133063267af96f
Tobias Glaubach <tobias.glaubach@asinco.de>
2018-11-17 19:52:57 +0100

    intermediate commit

--- PlottingHelpers.py
+++ PlottingHelpers.py
@@ -37,3 +37,31 @@ def plot_line(ser_high, ser_mid, ser_low, col, ax=None, fig=None):
     ax.plot(ser_mid.index, ser_mid.values, marker='o', color=col)
     ax.plot(ser_low.index, ser_low.values, marker='o', linestyle='dashed', color=col)
     
+def plot_imshow(df, resample=True):
+    cols_data = [col for col in df.columns if col.startswith('s')] 
+    fig, axes = plt.subplots(int(np.ceil(len(cols_data)/3)), 3, figsize=(17, 10))
+    axes = axes.flatten()
+
+    m = len(df['unit_nr'].unique())
+    n = 2 * m if resample else: df['time'].values.max()
+
+    t = np.linspace(0, 1, m)
+    dc_tmp = dict([ ( col, np.zeros((m, n)) ) for col in cols_data ])
+
+    for nr, d in df.groupby('unit_nr'):
+        tmp = ProcessingHelpers.resample_fixed(d, n) if resample else d
+
+        for ax, col in zip(axes, cols_data):
+            v = np.zeros((1, n))
+            v[:] = np.nan
+            v[0, (n - len(d)):] = tmp[col].values
+            dc_tmp[col][nr - 1, :] = v
+
+    for ax, col in zip(axes, cols_data):
+        mat = dc_tmp[col]
+        ax.imshow(mat)
+        ax.set_ylabel(col)
+        ax.grid(False)
+
+    for i in range(len(axes) - len(cols_data)):
+        fig.delaxes(axes[-(i+1)])
--- ProcessingHelpers.py
+++ ProcessingHelpers.py
@@ -33,3 +33,18 @@ def normalize_col_by_unit(df, col):
 
     df_sub[col] = rul_rel
     return df_sub
+
+def resample_fixed(df, n_new):
+    n_old, m = df.values.shape
+    mat_old = df.values
+    mat_new = np.zeros((n_new, m))
+    x_old = np.linspace(df.index.min(), df.index.max(), n_old)
+    x_new = np.linspace(df.index.min(), df.index.max(), n_new)
+        
+    for j in range(m):
+        y_old = mat_old[:, j]
+        y_new = np.interp(x_new, x_old, y_old)
+        mat_new[:, j] = y_new
+        
+    return pd.DataFrame(mat_new, index=x_new, columns=df.columns)
+    
